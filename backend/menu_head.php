273850d7823f275b12bd1337c5351b30bc23bc5b
Chayapat Niropas <papae_peez02@hotmail.co.th>
2023-03-13 11:55:52 +0700

    first push

--- backend/menu_head.php
+++ backend/menu_head.php
@@ -32,31 +32,30 @@
         <!-- Navbar Search -->
     </ul>
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto center" >
-        <li class="nav-item d-sm-inline-block">
-            <a href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+    <ul class="navbar-nav ml-auto center">
+        <li class="nav-item d-sm-inline-block" >
+            <a href="<?php echo PATH; ?>/backend/" class="nav-link">
                 <i class="nav-icon fas fa-globe"> ภาพรวม </i></a>
         </li>
         <li class="nav-item d-sm-inline-block">
-            <a href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+            <a href="<?php echo PATH; ?>/backend/" class="nav-link" style="cursor: no-drop;">
                 <i class="nav-icon fas fa-cash-register"> ขายสินค้า </i></a>
         </li>
         <li class="nav-item d-sm-inline-block">
-            <a href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+            <a href="<?php echo PATH; ?>/backend/" class="nav-link" style="cursor: no-drop;">
                 <i class="nav-icon fas fa-credit-card">&nbsp;ผ่อนสินค้า </i></a>
         </li>
         <li class="nav-item d-sm-inline-block">
-            <a href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+            <a href="<?php echo PATH; ?>/backend/" class="nav-link" style="cursor: no-drop;">
                 <i class="nav-icon fas fas fa-user-plus"> เพิ่มสมาชิก </i></a>
         </li>
         <li class="nav-item d-sm-inline-block">
-            <a href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+            <a href="<?php echo PATH; ?>/backend/" class="nav-link" style="cursor: no-drop;">
                 <i class="nav-icon fas fa-chart-pie"> รายงาน </i></a>
         </li>
-        &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
-        <li class="nav-item d-sm-inline-block front-danger">
-            <a type="button"href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
-            <p style="color:red"><i class="nav-icon fas  fa-sign-in-alt"> ออกจากระบบ </i></p>
+        <li class="nav-item d-sm-inline-block front-danger" style="margin-left:70px;">
+            <a type="button" href="<?php echo PATH; ?>/backend/logout.php" class="nav-link">
+                <p style="color:red"><i class="nav-icon fas  fa-sign-in-alt"> ออกจากระบบ </i></p>
             </a>
         </li>
     </ul>
--- backend/menu_left.php
+++ backend/menu_left.php
@@ -10,9 +10,9 @@
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex" >
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?php echo PATH; ?>/backend/img/default.jpg"  class="img-circle elevation-2" alt="User Image">
+                <img src="<?php echo PATH; ?>/backend/img/default.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block"><?php echo $_SESSION['firstname'].' '.$_SESSION['lastname'];?> </a>
@@ -21,44 +21,12 @@
 
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-                <li class="nav-item">
-                    <a href="<?php echo PATH; ?>/backend/customer" class="nav-link">
-                    <i class="nav-icon fas fa-address-book"></i>
-                        <p>
-                        ข้อมูลลูกค้า
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?php echo PATH; ?>/backend/user" class="nav-link">
-                        <i class="nav-icon fas fa-users"></i>
-                        <p>
-                        ข้อมูลพนักงาน
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?php echo PATH; ?>/backend/supplier" class="nav-link">
-                        <i class="nav-icon 	fas fa-user-tie"></i>
-                        <p>
-                        ข้อมูลเจ้าหนี้/ผู้ขาย
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="<?php echo PATH; ?>/backend/user" class="nav-link">
-                        <i class="nav-icon 	fas fa-comments-dollar"></i>
-                        <p>
-                        ข้อมูลลูกหนี้
-                        </p>
-                    </a>
-                </li>
+                <li class="nav-header">ระบบข้อมูลสินค้า</li>
                 <li class="nav-item">
                     <a href="<?php echo PATH; ?>/backend/inventory" class="nav-link">
                         <i class="nav-icon fas fa-paste"></i>
                         <p>
-                        ข้อมูลสินค้า
+                            ข้อมูลสินค้า
                         </p>
                     </a>
                 </li>
@@ -66,7 +34,7 @@
                     <a href="<?php echo PATH; ?>/backend/group" class="nav-link">
                         <i class="nav-icon 	fa fa-th-large"></i>
                         <p>
-                        หมวดสินค้า
+                            หมวดสินค้า
                         </p>
                     </a>
                 </li>
@@ -74,7 +42,7 @@
                     <a href="<?php echo PATH; ?>/backend/unit" class="nav-link">
                         <i class="nav-icon fa fa-cube"></i>
                         <p>
-                        หน่วยพัสดุ
+                            หน่วยพัสดุ
                         </p>
                     </a>
                 </li>
@@ -82,7 +50,7 @@
                     <a href="<?php echo PATH; ?>/backend/type" class="nav-link">
                         <i class="nav-icon 	far fa-object-group"></i>
                         <p>
-                        ประเภทสินค้า
+                            ประเภทสินค้า
                         </p>
                     </a>
                 </li>
@@ -90,7 +58,7 @@
                     <a href="<?php echo PATH; ?>/backend/brand" class="nav-link">
                         <i class="nav-icon fas fa-star"></i>
                         <p>
-                        แบรนด์สินค้า
+                            แบรนด์สินค้า
                         </p>
                     </a>
                 </li>
@@ -98,10 +66,44 @@
                     <a href="<?php echo PATH; ?>/backend/color" class="nav-link">
                         <i class="nav-icon 	fas fa-palette"></i>
                         <p>
-                        สีสินค้า
+                            สีสินค้า
                         </p>
                     </a>
                 </li>
+                <li class="nav-header">อยู่ระหว่างการพัฒนา</li>
+                <li class="nav-item">
+                    <a href="<?php echo PATH; ?>/backend/customer" class="nav-link">
+                        <i class="nav-icon fas fa-address-book"></i>
+                        <p>
+                            ข้อมูลลูกค้า
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo PATH; ?>/backend/user" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            ข้อมูลพนักงาน
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo PATH; ?>/backend/supplier" class="nav-link">
+                        <i class="nav-icon 	fas fa-user-tie"></i>
+                        <p>
+                            ข้อมูลเจ้าหนี้/ผู้ขาย
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo PATH; ?>/backend/user" class="nav-link">
+                        <i class="nav-icon 	fas fa-comments-dollar"></i>
+                        <p>
+                            ข้อมูลลูกหนี้
+                        </p>
+                    </a>
+                </li>
+
             </ul>
         </nav>
 
